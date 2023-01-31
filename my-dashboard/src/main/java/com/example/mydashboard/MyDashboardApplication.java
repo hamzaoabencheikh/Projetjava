a5feea99ee1dbb61192e13ae540ed145144b4b38
hamzaoabencheikh <115072857+hamzaoabencheikh@users.noreply.github.com>
2023-01-31 13:35:57 +0100

    Dashboard Eureka implementation

--- my-dashboard/src/main/java/com/example/mydashboard/MyDashboardApplication.java
+++ my-dashboard/src/main/java/com/example/mydashboard/MyDashboardApplication.java
@@ -1,13 +1,17 @@
 package com.example.mydashboard;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@EnableAdminServer
 public class MyDashboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyDashboardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyDashboardApplication.class, args);
+    }
 
 }
--- my-dashboard/src/test/java/com/example/mydashboard/MyDashboardApplicationTests.java
+++ my-dashboard/src/test/java/com/example/mydashboard/MyDashboardApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MyDashboardApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
